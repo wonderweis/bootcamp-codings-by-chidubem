f1594da0b368d7faaabf5495c63f9bca64dc59b0
wonderweis <emmanuelgerald05@gmail.com>
2017-04-08 08:09:27 +0100

    finished test coding and another run

---
+++ aritGeo/spec/testSpec.js
@@ -0,0 +1,53 @@
+(function(){
+  'use strict';
+
+  describe("Generate prime numbers in an array when given a value: ", function() {
+
+    describe("Case for an invalid value input", function() {
+
+      it("should return invalid for an invalid input", function() {
+        expect(aritGeo('abcdefghijklmnopqrstuvwxyz')).toEqual('Invalid');
+      });
+
+    });
+
+    describe("Case for a value less than or equal to zero", function() {
+
+      it("should return 'put num greater than zero' for 0", function() {
+        expect(aritGeo(0)).toEqual('put num greater than zero');
+      });
+
+      it("should return 'put num greater than zero' for -10", function() {
+        expect(aritGeo(-10)).toEqual('put num greater than zero');
+      });
+
+      it("should return 'put num greater than zero' for -135", function() {
+        expect(aritGeo(-135)).toEqual('put num greater than zero');
+      });
+    });
+
+    describe("Case for when no prime array is generated", function() {
+
+      it("should return [] for 1", function() {
+        expect(aritGeo(1)).toEqual([]);
+      });
+
+    });
+
+    describe("Case for when prime array is generated", function() {
+
+      it("should return [2, 3, 5, 7] for 7", function() {
+        expect(aritGeo(7)).toEqual([2, 3, 5, 7]);
+      });
+
+      it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47] for 50", function() {
+        expect(aritGeo(50)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]);
+      });
+
+      it("should return [2] for 2", function() {
+        expect(aritGeo(2)).toEqual([2]);
+      });
+      
+    });
+  });
+})();
